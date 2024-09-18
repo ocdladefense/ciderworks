a43893a4719ea03c926089c431ec9fb2548f4073
SullivanKE <Katelyn135@gmail.com>
2024-09-18 15:53:00 -0700

    Fixed issue where footer would unseat itself from the bottom of the page if there was not enough content.

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -44,7 +44,7 @@ export default function App() {
         <>
             {header}
             <Header />
-            <main>
+            <main class="grow">
                 {page}
             </main>
             <Footer />
