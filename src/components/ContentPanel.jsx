d0e0c48084da9a672b29703aa3dab19606d4b41e
SullivanKE <KatelynSullivan135@gmail.com>
2024-10-03 12:14:26 -0700

    Changed headings on mobile to be left justified, less bold, and uppercase.

--- src/components/ContentPanel.jsx
+++ src/components/ContentPanel.jsx
@@ -4,8 +4,8 @@ export default function TwoColumnPanel({ heading, children, striped = false }) {
 
     return (
         <section class={`tablet:grid px-8 grid-rows-5 gap-0 tablet:place-items-start tablet:mb-16 ${striped && "panel-striped"}`}>
-            <div class="container tablet:mb-8 row-span-1 text-center mx-auto laptop:text-left laptop:mx-0">
-                <h3 class="subpixel-antialiased mb-8 text-wb-cordovan text-4xl tablet:text-7xl laptop:text-6xl font-semibold">{heading}</h3>
+            <div class="container tablet:mb-8 row-span-1 mx-auto laptop:text-left laptop:mx-0">
+                <h3 class="subpixel-antialiased mb-8 text-wb-cordovan text-4xl tablet:text-7xl laptop:text-6xl content-heading">{heading}</h3>
             </div>
             <div class="laptop:container mb-32 laptop:mb-16 row-span-4">
                 {children}
--- src/components/TwoColumnPanel.jsx
+++ src/components/TwoColumnPanel.jsx
@@ -3,8 +3,8 @@ import { vNode, View } from "@ocdla/view";
 export default function TwoColumnPanel({ heading, children, order = "ltr", striped = false }) {
     return (
         <section class={`laptop:min-w-[800px] p-8 tablet:grid tablet:grid-rows-5 gap-0 tablet:place-items-start mb-16 tablet:my-22 ${striped && "panel-striped"}`}>
-            <div class="container row-span-1 text-center mx-auto laptop:text-left laptop:mx-0">
-                <h3 class="subpixel-antialiased mb-8 text-wb-cordovan text-4xl tablet:text-7xl laptop:text-6xl font-semibold">{heading}</h3>
+            <div class="container row-span-1 mx-auto laptop:text-left laptop:mx-0">
+                <h3 class="subpixel-antialiased mb-8 text-wb-cordovan text-4xl tablet:text-7xl laptop:text-6xl content-heading">{heading}</h3>
             </div>
 
             <div class="container-none tablet:grid laptop:container mx-auto h-full laptop:grid-cols-5 gap-0 laptop:gap-8 tablet:grid-flow-row row-span-4">
